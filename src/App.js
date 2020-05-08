21b935e509de965230846db7f3012b06d94335ae
mtq0205 <soumik1978@yahoo.co.in>
2020-05-08 21:04:48 +0530

    feature branch first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Do not Edit this line when needed.
+          Edit <code>src/App.js</code> and save to reload. Do not Edit this line when needed. I was changed in feature branch....
         </p>
         <a
           className="App-link"
