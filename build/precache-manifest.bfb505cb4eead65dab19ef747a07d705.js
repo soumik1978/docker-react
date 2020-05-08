b10a3c9d0049326f47ca38d9a70b3079547d608d
mtq0205 <soumik1978@yahoo.co.in>
2020-05-08 10:45:43 +0530

    test

---
+++ build/precache-manifest.bfb505cb4eead65dab19ef747a07d705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb247d37fc9e2e35b778dbacfa4d473",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6ee5f045a60585c89b0",
+    "url": "/static/css/main.a61434c5.chunk.css"
+  },
+  {
+    "revision": "b5b734056f93cf4f02d3",
+    "url": "/static/js/2.b1327c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1327c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ee5f045a60585c89b0",
+    "url": "/static/js/main.6c755f35.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Do not Edit this line when needed.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React for some real fun!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
